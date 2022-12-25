44234ab9fe95967a27cdfe42a0fefdeb9bab3bc0
Ayoub Httater <ayoub.httater@terabyte-software.com>
2022-12-25 19:53:30 +0100

    Add project files.

---
+++ Bundles.Solution.Test/BundleDomainTest.cs
@@ -0,0 +1,126 @@
+﻿namespace Bundles.Solution.Test;
+
+public class BundleDomainTest
+{
+    [Fact]
+    public void Create_bundle_with_empty_name_should_throw_exception()
+    {
+        //Arrange
+        string name = " ";
+        string expectedMssage = "Name shouldn't be null or empty";
+
+        //Act - Assert
+        var exception = Assert.Throws<ArgumentException>(() => new Bundle(name, true, 1));
+        Assert.StartsWith(expectedMssage, exception.Message);
+        Assert.Equal(nameof(name), exception.ParamName);
+    }
+
+    [Fact]
+    public void Create_bundle_with_number_of_units_equal_zero_should_throw_exception()
+    {
+        //Arrange
+        int numberOfUnits = 0;
+        string expectedMssage = "Number of units shouldn't be less then 1";
+
+        //Act - Assert
+        var exception = Assert.Throws<ArgumentException>(() => new Bundle("Seat", true, numberOfUnits));
+        Assert.StartsWith(expectedMssage, exception.Message);
+        Assert.Equal(nameof(numberOfUnits), exception.ParamName);
+    }
+
+    [Fact]
+    public void Create_bundle_as_non_raw_material_with_no_bundle_parts_should_throw_exception()
+    {
+        //Arrange
+        List<Bundle> bundleParts = default!;
+        string expectedMssage = "Bundle parts shouldn't be less then 1 for non raw material";
+
+        //Act - Assert
+        var exception = Assert.Throws<ArgumentException>(() => new Bundle("Seat", false, 1, bundleParts));
+        Assert.StartsWith(expectedMssage, exception.Message);
+        Assert.Equal(nameof(bundleParts), exception.ParamName);
+    }
+
+    [Fact]
+    public void Create_bundle_as_raw_material_should_success()
+    {
+        //Arrange - Act
+        var frame = new Bundle("Frame", true, 1);
+
+        //Assert
+        Assert.NotNull(frame);
+        Assert.Empty(frame.BundleParts);
+    }
+
+    [Fact]
+    public void Create_bundle_as_non_raw_material_should_success()
+    {
+        //Arrange
+        var frame = new Bundle("Frame", true, 1);
+        var tube = new Bundle("Tube", true, 1);
+
+        //Act
+        var wheel = new Bundle("Wheel", false, 2, new List<Bundle> { frame, tube });
+
+        //Assert
+        Assert.NotNull(wheel);
+        Assert.NotNull(wheel.BundleParts);
+        Assert.NotEmpty(wheel.BundleParts);
+    }
+
+    [Fact]
+    public void Raw_material_inventory_should_success()
+    {
+        //Arrange
+        Bundle bike = CreateBikeBundle();
+        var expectedResult = new List<(string Name, int Count)>
+        {
+            ("Seat",1),
+            ("Pedal",2),
+            ("Frame",2),
+            ("Tube",2),
+        };
+
+        //Act
+        List<(string Name, int Count)> rawMaterials = bike.RawMaterialInventory();
+
+        //Assert
+        Assert.Equal(expectedResult, rawMaterials);
+    }
+
+    [Fact]
+    public void Get_maximum_number_of_finished_bundles_should_success()
+    {
+        //Arrange
+        Bundle bike = CreateBikeBundle();
+
+        var rawMaterialsStock = new List<(string Name, int Count)>
+        {
+            ("Seat",50),
+            ("Pedal",60),
+            ("Frame",60),
+            ("Tube",35),
+        };
+
+        int expectedResult = 17;
+
+        //Act
+        var result = bike.GetMaximumNumberOfFinishedBundles(rawMaterialsStock);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    private static Bundle CreateBikeBundle()
+    {
+        var seat = new Bundle("Seat", true, 1);
+        var pedal = new Bundle("Pedal", true, 2);
+
+        var frame = new Bundle("Frame", true, 1);
+        var tube = new Bundle("Tube", true, 1);
+        var wheel = new Bundle("Wheel", false, 2, new List<Bundle> { frame, tube });
+
+        var bike = new Bundle("Bike", false, 1, new List<Bundle> { seat, pedal, wheel });
+        return bike;
+    }
+}
---
+++ Bundles.Solution/BundleAggregate/Bundle.cs
@@ -0,0 +1,55 @@
+﻿namespace Bundles.Solution.BundleAggregate;
+
+public class Bundle
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = default!;
+    public bool IsRawMaterial { get; private set; }
+    public int NumberOfUnits { get; private set; }
+
+    // DDD Patterns comment
+    // Using a private collection field, better for DDD Aggregate's encapsulation
+    // so Bundles cannot be added from "outside the AggregateRoot" directly to the collection.
+    private readonly List<Bundle> _bundleParts = new();
+    public IReadOnlyCollection<Bundle> BundleParts => _bundleParts;
+
+    private Bundle() { }
+
+    public Bundle(string name, bool isRawMaterial, int numberOfUnits, List<Bundle> bundleParts = default!)
+    {
+        // Since we don't have a validation layer I will just put all my validation in the constructor
+        Name = !string.IsNullOrWhiteSpace(name) ? name : throw new ArgumentException("Name shouldn't be null or empty", nameof(name));
+        IsRawMaterial = isRawMaterial;
+        NumberOfUnits = numberOfUnits > 0 ? numberOfUnits : throw new ArgumentException("Number of units shouldn't be less then 1", nameof(numberOfUnits));
+        if (!isRawMaterial) _bundleParts = bundleParts?.Count > 0 ? bundleParts : throw new ArgumentException("Bundle parts shouldn't be less then 1 for non raw material", nameof(bundleParts)); ;
+    }
+
+    public List<(string Name, int Count)> RawMaterialInventory(List<(string name, int count)> rawMaterials = default!, int NumberOfParentUnits = 1)
+    {
+        rawMaterials ??= new();
+
+        // In case of raw material we inventory the number of units (raw material does not have bundle parts)
+        if (IsRawMaterial)
+        {
+            rawMaterials.Add((Name, NumberOfParentUnits * NumberOfUnits));
+            return rawMaterials;
+        }
+
+        // In case of non raw material we will go forward to inventory the number of units of bundle parts
+        _bundleParts.ForEach(bundlePart => bundlePart.RawMaterialInventory(rawMaterials, NumberOfParentUnits * NumberOfUnits));
+        return rawMaterials;
+    }
+
+    public int GetMaximumNumberOfFinishedBundles(List<(string Name, int Count)> stockRawMaterials)
+    {
+        List<(string Name, int Count)> rawMaterials = RawMaterialInventory();
+
+        //To obtain the maximum number of finished bundles is sufficient to incorporate the consumption rate
+        //then the prodaction of bundles will stop at the first exhausted raw material 
+        var maximumNumberOfFinishedBundles = (from rawMaterial in rawMaterials
+                                              join stockRawMaterial in stockRawMaterials on rawMaterial.Name equals stockRawMaterial.Name
+                                              select stockRawMaterial.Count/rawMaterial.Count).Min();
+
+        return maximumNumberOfFinishedBundles;
+    }
+}
